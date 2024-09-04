b43c723ffaf9bc6268623c3f811dcc0d47d92767
Bayan Dzehverovic <bayanbaker@gmail.com>
2024-09-04 13:20:43 -0400

    Added interview questions number 8 and 13

---
+++ src/Utils/Bayan_JIQ_008.java
@@ -0,0 +1,16 @@
+package Utils;
+
+public class Bayan_JIQ_008 {
+
+    /*
+    What is the difference between a class and an object?
+
+    A class is like a blueprint. An object is the actual result of the class:
+
+    blueprint --> building
+
+    "Class" goes into a java file and "objects" are what goes into a class.
+
+     */
+
+}
