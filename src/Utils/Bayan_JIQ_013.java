565293591be50ec8df3ff2c29b9531fe56e0e2f1
Bayan Dzehverovic <bayanbaker@gmail.com>
2024-09-04 13:20:43 -0400

    Added interview questions number 8 and 13

---
+++ src/Utils/Bayan_JIQ_013.java
@@ -0,0 +1,17 @@
+package Utils;
+
+public class Bayan_JIQ_013 {
+
+    /*
+    How can you access the last element in an array?
+
+    Indexes start at 0 (zero).
+
+    First: know how to check the number of elements: arr.length
+
+    Second: know how to read the last index (regardless of number of elements): length - 1
+
+    Third: put them together: arr[arr.length-1] --> will help you read the last index
+
+     */
+}
