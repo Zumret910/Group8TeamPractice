b13822605f5e098cb4307ebcf4796c44794adea2
16so03 <laumovas@gmail.com>
2024-09-04 19:19:04 -0700

    Added one_two class

---
+++ src/Utils/one_two.java
@@ -0,0 +1,9 @@
+package Utils;
+
+public class one_two {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
