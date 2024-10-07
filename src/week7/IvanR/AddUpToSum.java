81d485dd57f348ada836e0b8771ded779349fa32
Ivan_Cydeo <remezivan1@gmail.com>
2024-10-07 10:08:23 -0400

    Week 7 added

---
+++ src/week7/IvanR/AddUpToSum.java
@@ -0,0 +1,24 @@
+package week7.IvanR;
+
+public class AddUpToSum {
+
+    public static void main(String[] args) {
+
+
+    }
+}
+/*
+Add Up to Sum
+
+Given an int array and an int representing the sum number, create a program that can find the pair of numbers from the array that add up to the sum number
+
+Add the numbers from the array to determine the possible pairs that add up to the sum
+
+Ex:
+    array = [8, 7, 2, 5, 3, 1]
+    sum = 10
+
+    output:
+        8 & 2
+        7 & 3
+*/
---
+++ src/week7/IvanR/MaxNumber.java
@@ -0,0 +1,23 @@
+package week7.IvanR;
+
+public class MaxNumber {
+
+    public static void main(String[] args) {
+
+
+    }
+}
+ /*
+Max Number
+
+Create a method that will find and return the max number in an int array
+    parameter: int[]
+    return: int
+
+The max number is the biggest value in the array
+
+Ex:
+    input:
+        max(3, 5, 1, 5, -1, 5)
+    output:
+*/
