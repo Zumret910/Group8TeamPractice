1e765104a58812a4483fe375e6a432014d0f7817
Ivan_Cydeo <remezivan1@gmail.com>
2024-10-07 13:29:30 -0400

    Week 7 added

--- src/week7/IvanR/AddUpToSum.java
+++ src/week7/IvanR/AddUpToSum.java
@@ -4,13 +4,35 @@ public class AddUpToSum {
 
     public static void main(String[] args) {
 
+        int[] arr = {8, 7, 2, 5, 3, 1};
 
+        addUpToSum(arr, 10);
+        System.out.println("-----------");
+        addUpToSum(arr, 8);
+        System.out.println("-----------");
+        addUpToSum(arr, 9);
+
+    }
+
+    public static void addUpToSum(int[] arr, int sum) {
+
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] + arr[j] == sum) {
+
+                    System.out.println(arr[i] + " & " + arr[j]);
+                }
+            }
+        }
     }
 }
 /*
 Add Up to Sum
 
-Given an int array and an int representing the sum number, create a program that can find the pair of numbers from the array that add up to the sum number
+Given an int array and an int representing the sum number,
+create a program that can find the pair of numbers from the array that add up to the sum number
 
 Add the numbers from the array to determine the possible pairs that add up to the sum
 
